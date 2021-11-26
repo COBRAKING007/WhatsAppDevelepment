bcd7c129c8f4ae65b86778e430d0206bd956fd00
DineshVenkatesan <95073411+DineshVenkatesan@users.noreply.github.com>
2021-11-26 14:04:03 +0530

    Facebook browser launch only

--- FBLogin.java
+++ FBLogin.java
@@ -11,35 +11,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FBLogin {
 	WebDriver driver;
-	@BeforeClass
+	@Test
 	public void launch() {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
+		driver.quit();
 	}
-	
-	@Test
-	public void tc01() {
-		driver.findElement(By.id("email")).sendKeys("Hi");
-		driver.findElement(By.id("pass")).sendKeys("Hello");
-		driver.findElement(By.id("u_0_d_zh")).click();
-	}
-	
-//	@Test(priority = 2, dataProvider = "MyData")
-//	public void reEnter(String email,String pass) {
-//		driver.navigate().refresh();
-//		driver.findElement(By.id("email")).sendKeys(email);
-//		driver.findElement(By.id("pass")).sendKeys(pass);
-//		driver.findElement(By.id("u_0_d_zh")).click();
-//		
-//	}
-	
-//	@DataProvider(name = "MyData")
-//	public Object[][] getDatas() {
-//		return new Object[][] {
-//			{"Jack","1234"},{"Michel","5678"},{"Dany","1290"}
-//		};
-	}
+}
 	
 
